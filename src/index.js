444f56da9ed11e2153d6da2f7d912d71b56742dd
ayakamiyake7 <88525743+ayakamiyake7@users.noreply.github.com>
2021-11-16 19:32:40 +0900

    Add todo practice.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,76 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  //to get value of textbox.
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  createIncompleteList(inputText);
+
+  // delete from incomplete area
+  const deleteFromIncompleteList = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
+  };
+
+  // function (add to incomplete function)
+  const createIncompleteList = (text) => {
+    //creating div
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    //creating li
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //creating complete button
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+      // delete elements
+      deleteFromIncompleteList(completeButton.parentNode);
+      // add elements
+      const addTarget = completeButton.parentNode;
+      const text = addTarget.firstElementChild.innerText;
+
+      addTarget.textContent = null;
+      const li = document.createElement("li");
+      li.innerText = text;
+
+      //back button
+      const backButton = document.createElement("button");
+      backButton.innerText = "戻す";
+      backButton.addEventListener("click", () => {
+        const deleteTarget = backButton.parentNode;
+        document.getElementById("complete-list").removeChild(deleteTarget);
+        // get text
+        const text = backButton.parentNode.firstElementChild.innerText;
+        console.log(text);
+      });
+
+      // set elements under the div
+      addTarget.appendChild(li);
+      addTarget.appendChild(backButton);
+
+      document.getElementById("complete-list").appendChild(addTarget);
+    });
+
+    //creating delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    deleteButton.addEventListener("click", () => {
+      // delete elements
+      deleteFromIncompleteList(deleteButton.parentNode);
+    });
+
+    // set elements under the div
+    div.appendChild(li);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+
+    document.getElementById("incomplete-list").appendChild(div);
+  };
+};
+
+document.getElementById("add-button").addEventListener("click", () => {
+  onClickAdd();
+});
